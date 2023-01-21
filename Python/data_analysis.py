9d233952d88cd59b31ad22ba4b8f3e8b65b1ce8c
Kyu Park <gypark23@uchicago.edu>
2023-01-21 17:28:04 -0600

    summarize, visualize part

--- Python/data_analysis.py
+++ Python/data_analysis.py
@@ -1,7 +1,62 @@
+import pandas as pd
+import os
+import glob
+import matplotlib.pyplot as plt
+
 def summarize_sensor_trace(csv_file: str):
+    df = pd.read_csv(csv_file)
+    vals = []
+    names = []
+    for (columnName, columnData) in df.iteritems():
+        if columnName in ('time', 'Unnamed: 37'):
+            continue
+        
+        names.append(columnName)
+        vals.append([columnData.mean(), columnData.var()])
+        # print("Mean of " + columnName + ": " + str(columnData.mean()))
+        # print("Variance of " + columnName + ": " + str(columnData.var()))
+    
+    ret = pd.DataFrame(vals, columns = ['Mean', 'Variance'], index = names)
+    print(ret)
+    return ret
+    
     pass
 
 def visualize_sensor_trace(csv_file: str, attribute: str):
+    df = pd.read_csv(csv_file)
+    ax = df.plot(x = 'time', y = attribute)
+    ax.set_xlabel("Time (milliseconds)")
+    ax.set_ylabel(attribute)
+    plt.show()
+    pass
+            
+        
+
+
+# summarize_sensor_trace("../Data/Lab1/JOG_P1_01.csv")
+# visualize_sensor_trace("../Data/Lab1/JOG_P1_01.csv", 'controller_left_vel.x')
+# Define additional functions as needed here!
+
+def combine_samples(activity: str, attribute: str):
+    # get all csv files
+    path = "../Data/Lab1/"
+    csv_files = glob.glob(os.path.join(path, "*.csv"))
+    df_list = []
+    # combine data for activity of interest into one df
+    for file in csv_files:
+        df = pd.read_csv(file)
+
+        name = file.split("\\")[-1].split('_')[0].rsplit('/', 1)[-1]
+        print(name)
+        if name == activity:
+            df_list.append(df)
+    activity_df = pd.concat(df_list)
+
+
+    ax = activity_df.plot(x = 'time', y = attribute)
+    ax.set_xlabel("Time (milliseconds)")
+    ax.set_ylabel(attribute)
+    plt.show()
     pass
 
-# Define additional functions as needed here!
+combine_samples("JOG", "controller_left_vel.x")
