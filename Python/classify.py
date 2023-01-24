573989c0a50e1dd365e37553ae7c23e26c6b5902
Dingyi Sun <45937900+DY-Acht@users.noreply.github.com>
2023-01-24 11:55:26 -0600

    Update classify.py
    final version of classifier codes

--- Python/classify.py
+++ Python/classify.py
@@ -26,7 +26,6 @@ class Activity:
 		self.mean_weights = np.array(36*[1])
 		self.var_weights = np.array(36*[1])
 
-
 	def update_weights(self, new_weights):
 		try:
 			self.mean_superweight =  new_weights[0]
@@ -36,21 +35,20 @@ class Activity:
 		except:
 			print("Error: wrong dimension!")
 
-
 	def print_weights(self):
-		print("Superweights: ", self.mean_superweight, self.var_superweight)
-		print("Mean weights: ", self.mean_weights)
-		print("Var weights: ", self.var_weights)
-
-
+		print("Mean weights: ", self.mean_superweight*self.mean_weights)
+		print("Var weights:  ", self.var_superweight*self.var_weights)
 
 	def compare(self, activity_df):
 		mean_array = activity_df['Mean'].to_numpy()
 		var_array = activity_df['Variance'].to_numpy()
+		
+		mean_dist = ((mean_array - self.mean_array)/self.mean_array)**2
+		var_dist = ((var_array - self.var_array)/self.var_array)**2
 
-		mean_dist = (((mean_array - self.mean_array)**2) * self.mean_weights).sum()
-		var_dist = (((var_array - self.var_array)**2) * self.var_weights).sum()
-		dist = mean_dist*self.mean_superweight + var_dist*self.var_superweight 
+		dist = 0
+		dist += self.mean_superweight * (mean_dist * self.mean_weights).sum()
+		dist += self.var_superweight * (var_dist * self.var_weights).sum()
 		return dist
 
 
@@ -70,22 +68,47 @@ class Classifier:
 		for activity in self.activities:
 			activity.update_weights(new_weights)
 
+	def print_weights(self):
+		try:
+			self.activities[0].print_weights()
+		except:
+			print("Error: no activity loaded!")
+
 	def classify(self, activity_df):
 		dists = []
 		for activitiy in self.activities:
 			dists.append(activitiy.compare(activity_df))
 
 		dists = np.array(dists)
-		result = self.names[np.argmin(dists)]
-		return result
+		category = self.names[np.argmin(dists)]
+		dists = np.sort(dists)
+
+		return category, dists
+
+
+def test(classifier, tasks, test_name, show_accuracy = True):
+	print("")
+	print(test_name)
+
+	hit = 0
+	for task in tasks:
+		task_df = DA.summarize_sensor_trace(dir + task)
+		category, dists = classifier.classify(task_df)
+
+		if task[0:3] == category:
+			hit += 1
+
+		print("    Activity ", task, " is classified as: ", category, end="  ")
+		print("    Dists = ", [int(dist) for dist in dists])
+	
+	if show_accuracy:
+		accuracy = hit / len(tasks)
+		print("\n    Accuracy: ", accuracy, "\n")
 
 
 
 
-
-
-
-
+# =================================================== testing ==================================================
 
 
 # building a list of activities
@@ -102,14 +125,32 @@ mean_superweight =  1
 mean_weights = 36*[1]
 var_superweight =  1
 var_weights = 36*[1]
+
+# raise the importance of head pos mean
+mean_weights[6] = 3
+mean_weights[7] = 8
+mean_weights[8] = 3
+
+# raise the importance of head pos var
+var_weights[6] = 3
+
+# raise the importance of controller rot, pos var
+var_weights[21] = 3
+var_weights[33] = 3
+
+# build list of weights
 new_weights = [mean_superweight, mean_weights, var_superweight, var_weights]
+
 C = Classifier(activities)
 C.update_weights(new_weights)
+C.print_weights()
 
 
-# testing
-tasks = ["JOG_P2_04.csv", "STD_P3_02.csv", "STR_P2_05.csv", "OHD_P1_04.csv", "TWS_P3_04.csv", "SIT_P3_01.csv"]
-for task in tasks:
-	task_df = DA.summarize_sensor_trace(dir + task)
-	res = C.classify(task_df)
-	print("Activity ", task, " is classified as: ", res)
+
+# testing 1
+tasks = ["JOG_P2_04.csv", "STD_P3_05.csv", "STR_P2_05.csv", "OHD_P1_04.csv", "TWS_P3_04.csv", "SIT_P1_04.csv"]
+test(C, tasks, "Test 1")
+
+# testing 2
+tasks = ["JOG_P1_04.csv", "STD_P1_01.csv", "STR_P3_05.csv", "OHD_P3_04.csv", "TWS_P2_04.csv", "SIT_P1_02.csv"]
+test(C, tasks, "Test 2")
