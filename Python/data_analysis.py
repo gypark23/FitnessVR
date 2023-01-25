7b069dbbd50951ef588b5a9075c62a80ea60db11
Kendrick Xie <kendrickxie@gmail.com>
2023-01-24 19:09:08 -0600

    code cleanup and make graph labels more clear

--- Python/data_analysis.py
+++ Python/data_analysis.py
@@ -17,11 +17,9 @@ def summarize_sensor_trace(csv_file: str):
         
         names.append(columnName)
         vals.append([columnData.mean(), columnData.var()])
-        # print("Mean of " + columnName + ": " + str(columnData.mean()))
-        # print("Variance of " + columnName + ": " + str(columnData.var()))
     
     ret = pd.DataFrame(vals, columns = ['Mean', 'Variance'], index = names)
-    # print(ret)
+    print(ret)
     # returns a dataframe of means and variances of each attribute
     return ret
     
@@ -50,32 +48,12 @@ def visualize_sensor_trace(csv_file: str = "", attribute: str = "", single_attri
         dataframes = [Standing, Sitting, Jogging, Stretching, Overhead, Twisting]
 
         # Output graphs for variance of different activities
-        compare_attributes(dataframes, 0, 3, "Mean")
-        compare_attributes(dataframes, 3, 6, "Mean")
-        compare_attributes(dataframes, 6, 9, "Mean")
-        compare_attributes(dataframes, 9, 12, "Mean")
-        compare_attributes(dataframes, 12, 15, "Mean")
-        compare_attributes(dataframes, 15, 18, "Mean")
-        compare_attributes(dataframes, 18, 21, "Mean")
-        compare_attributes(dataframes, 21, 24, "Mean")
-        compare_attributes(dataframes, 24, 27, "Mean")
-        compare_attributes(dataframes, 27, 30, "Mean")
-        compare_attributes(dataframes, 30, 33, "Mean")
-        compare_attributes(dataframes, 33, 36, "Mean")
+        for i in range(12):
+            compare_attributes(dataframes, i*3, i*3+3, "Mean")
 
         # Output graphs for variance of different activities
-        compare_attributes(dataframes, 0, 3, "Variance")
-        compare_attributes(dataframes, 3, 6, "Variance")
-        compare_attributes(dataframes, 6, 9, "Variance")
-        compare_attributes(dataframes, 9, 12, "Variance")
-        compare_attributes(dataframes, 12, 15, "Variance")
-        compare_attributes(dataframes, 15, 18, "Variance")
-        compare_attributes(dataframes, 18, 21, "Variance")
-        compare_attributes(dataframes, 21, 24, "Variance")
-        compare_attributes(dataframes, 24, 27, "Variance")
-        compare_attributes(dataframes, 27, 30, "Variance")
-        compare_attributes(dataframes, 30, 33, "Variance")
-        compare_attributes(dataframes, 33, 36, "Variance")
+        for i in range(12):
+            compare_attributes(dataframes, i*3, i*3+3, "Variance")
                 
     pass
             
@@ -112,26 +90,9 @@ def combine_samples(activity: str):
     print(activity_df)
 
     return activity_df
-    # for (columnName, columnData) in activity_df.iteritems():
-    #     if columnName in ('time', 'Unnamed: 37'):
-    #         continue
-        
-    #     names.append(columnName)
-    #     vals.append([columnData.mean(), columnData.var()])
 
-    # ret = pd.DataFrame(vals, columns = ['Mean', 'Variance'], index = names)
-    # print(ret)
-    # return ret
-
-# STD	Standing	VideoLinks to an external site.
-# SIT	Sitting	VideoLinks to an external site.
-# JOG	Jogging	VideoLinks to an external site.
-# STR	Arms stretching	VideoLinks to an external site.
-# OHD	Arms overhead	VideoLinks to an external site.
-# TWS	Twisting
 
 def compare_attributes(dataframes, start, end, stat):
-
     # Get the number of indexes in a dataframe
     num_indexes = end - start
     # Create a list of colors for the bars
@@ -157,7 +118,7 @@ def compare_attributes(dataframes, start, end, stat):
         plt.bar(r + i*barWidth, means, color=colors[i%6], width=barWidth, edgecolor='white', label=activities[i])
 
     # Add labels to the x-axis
-    plt.xticks(r + len(dataframes)*barWidth/2, dataframes[0].index[start:end])
+    plt.xticks(r + len(dataframes)*barWidth/2, dataframes[0].index[start:end], rotation=-5)
 
     # Add a y-axis label
     plt.ylabel(stat)
@@ -167,7 +128,6 @@ def compare_attributes(dataframes, start, end, stat):
 
     # Display the bar chart
     plt.show()
-
 
 # use visualize_sensor_trace again, but this time print a summary of comparison of multiple attributes and activities
 visualize_sensor_trace(single_attribute_mode = False)
