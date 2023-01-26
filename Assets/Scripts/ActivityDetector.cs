2b07f5c833b79c1eec198aea628007fdd56f2c80
Spencer Ng <sng1488@gmail.com>
2023-01-26 02:02:17 -0600

    Add lab 2 skeleton code and train set

--- Assets/Scripts/ActivityDetector.cs
+++ Assets/Scripts/ActivityDetector.cs
@@ -1,15 +1,30 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ActivityDetector : MonoBehaviour
 {
+    // Feel free to add additional class variables here
     OculusSensorReader sensorReader;
 
     // Start is called before the first frame update
     void Start()
     {
         sensorReader = new OculusSensorReader();
+    }
+
+    /// <summary>
+    /// Return the activity code of the current activity. Feel free to add
+    /// additional parameters to this function.
+    /// </summary>
+    string GetCurrentActivity(Dictionary<string, Vector3> attributes)
+    {
+        // Here's an example of fetching an attribute from the dictionary
+        var headsetVelX = attributes["headset_vel"].x;
+
+        // TODO: Implement your algorithm here to determine the current activity based
+        // on recent sensor traces.
+
+        return "Unknown";
     }
 
     // Update is called once per frame
@@ -21,12 +36,8 @@ public class ActivityDetector : MonoBehaviour
         // and Vector3 objects as values
         var attributes = sensorReader.GetSensorReadings();
 
-        // Here's an example of fetching an attribute from the dictionary
-        var headsetVelX = attributes["headset_vel"].x;
+        var currentActivity = GetCurrentActivity(attributes);
 
-        // Implement your algorithm here to determine the current activity based
-        // on recent sensor traces
-
-        // Update the Activity Sign text based on the detected activity
+        // TODO: Update the Activity Sign text based on the detected activity
     }
 }
