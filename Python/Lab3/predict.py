cf1728fe774298e163b1e32befb92915c55d027c
Kyu Park <gypark23@uchicago.edu>
2023-02-21 19:09:41 -0600

    added random forest figure, important feature

--- Python/Lab3/predict.py
+++ Python/Lab3/predict.py
@@ -11,7 +11,8 @@ from preprocess import create_dataset
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import f1_score
-
+import matplotlib.pyplot as plt
+from sklearn.tree import plot_tree
 dir_1 = "../../Data/Lab3/Train/"
 dir_2 = "../../Data/Lab3/Test/"
 
@@ -34,9 +35,6 @@ accuracy = accuracy_score(new_labels, pred_labels)
 conf_matrix = confusion_matrix(new_labels, pred_labels)
 
 
-
-
-
 end_time = time.time()
 
 print(f"Training time: {train_time - start_time} seconds")
@@ -47,8 +45,31 @@ print("F1 Score:", f1_score(new_labels, pred_labels))
 print(conf_matrix)
 
 
+# Export
 
+features =  [
+   "head_norms",
+	"head_osc", 
+	"head_angv_x", 
+	"head_angv_y",
+	"head_angv_z", 
 
+	"left_norms", 
+	"left_angv_x",
+	"left_angv_y", 
+	"left_angv_z",
+
+	"right_norms", 
+	"right_angv_x", 
+	"right_angv_y",
+	"right_angv_z",
+]
+
+attr = [f + "_"+e for f in features for e in ["Mean", "Var", "Quantile", "Entropy"]]
+
+plt.figure(figsize=(20,20))
+plot_tree(rf.estimators_[0], filled=True, rounded=True, feature_names = attr, class_names = ["Male", "Female"])
+plt.savefig("../../Reports/random_forest.png")
 
 
 
